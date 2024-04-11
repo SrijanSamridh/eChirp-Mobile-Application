4bc9461b86972433a57fb904219f3ac572aacf0a
ravneet <ravneet1805.be21@chitkara.edu.in>
2024-04-11 22:39:09 +0530

    Added Event Detail Page

--- lib/screens/group/components/group_detail.dart
+++ lib/screens/group/components/group_detail.dart
@@ -30,187 +30,183 @@ class _GroupDetailsPageState extends State<GroupDetailsPage> {
           )
         ],
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 120),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                height: size.height * 0.32,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  color: GlobalVariables.chatBubbleColor,
-                ),
-                padding: EdgeInsets.all(20.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      height: 15,
-                    ),
-                    CircleAvatar(
-                      radius: 50,
-                      backgroundColor: Colors.white,
-                    ),
-                    SizedBox(height: 10.0),
-                    Text(
-                      'Football Lovers',
-                      style: TextStyle(
-                        fontSize: 36.0,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    Text(
-                      'Group: 22.3k members',
-                      style: TextStyle(fontSize: 13),
-                    ),
-                  ],
-                ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 100),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              height: size.height * 0.28,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: GlobalVariables.chatBubbleColor,
               ),
-              SizedBox(height: 20),
-              Container(
-                height: 250,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  color: Colors.white,
-                ),
-                //padding: EdgeInsets.all(20.0)
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 10.0),
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Container(
-                            width: 40,
-                            height: 40,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(50),
-                                //shape: BoxShape.circle,
-                                color: Colors.grey[300]),
-                            child: Center(
-                                child: Icon(Icons.people_rounded,
-                                    color: GlobalVariables.kPrimaryColor))),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text("Add People to Group",
-                            style: TextStyle(
-                                color: GlobalVariables.kPrimaryColor,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16))
-                      ],
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  CircleAvatar(
+                    radius: size.width * 0.12,
+                    backgroundColor: Colors.white,
+                  ),
+                  SizedBox(height: 10.0),
+                  Text(
+                    'Football Lovers',
+                    style: TextStyle(
+                      fontSize: 34.0,
+                      fontWeight: FontWeight.w500,
                     ),
-                    Divider(),
-                    SizedBox(height: 10.0),
-                    Expanded(
-                      child: ListView.builder(
-                        itemCount: 5,
-                        itemBuilder: (context, index) {
-                          return _buildMemberTile("Richard james");
-                        },
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                  Text(
+                    'Group: 22.3k members',
+                    style: TextStyle(fontSize: 13),
+                  ),
+                ],
               ),
-              SizedBox(height: 20),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  color: Colors.white,
-                ),
-                padding: EdgeInsets.all(10.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 10.0),
-                    _buildActionTile('Clear Chat'),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 18.0),
-                      child: Divider(
-                        color: Colors.grey,
-                        height: 0,
-                      ),
-                    ),
-                    _buildActionTile('Exit Group'),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 18.0),
-                      child: Divider(
-                        color: Colors.grey,
-                        height: 0,
-                      ),
-                    ),
-                    _buildActionTile('Report Group'),
-                  ],
-                ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              height: size.height * 0.28,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white,
               ),
-              Container(
-                padding: EdgeInsets.all(20.0),
-                alignment: Alignment.bottomLeft,
-                child: Text(
-                  'Created 13 June 2023',
-                  style: TextStyle(fontSize: 14.0, color: Colors.grey[700]),
-                ),
+              //padding: EdgeInsets.all(20.0)
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 10.0),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                          width: 40,
+                          height: 40,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              //shape: BoxShape.circle,
+                              color: Colors.grey[300]),
+                          child: Center(
+                              child: Icon(Icons.people_rounded,
+                                  color: GlobalVariables.kPrimaryColor))),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text("Add People to Group",
+                          style: TextStyle(
+                              color: GlobalVariables.kPrimaryColor,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16))
+                    ],
+                  ),
+                  Divider(),
+                  SizedBox(height: 10.0),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: 10,
+                      itemBuilder: (context, index) {
+                        return _buildMemberTile("Richard james");
+                      },
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              height: size.height * 0.2,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white,
+              ),
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 5.0),
+                  _buildActionTile('Clear Chat'),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                    child: Divider(
+                      color: Colors.grey,
+                      height: 0,
+                    ),
+                  ),
+                  _buildActionTile('Exit Group'),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                    child: Divider(
+                      color: Colors.grey,
+                      height: 0,
+                    ),
+                  ),
+                  _buildActionTile('Report Group'),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(20.0),
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                'Created 13 June 2023',
+                style: TextStyle(fontSize: 14.0, color: Colors.grey[700]),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 
   Widget _buildMemberTile(String title) {
-    return Column(
-      children: [
-        Container(
-          margin: EdgeInsets.symmetric(vertical: 10.0),
-          child: Row(
-            children: [
-              SizedBox(
-                width: 10,
-              ),
-              CircleAvatar(
-                backgroundColor: Colors.black,
-              ),
-              SizedBox(width: 10.0),
-              Column(
-                children: [
-                  Text(
-                    title,
-                    style: TextStyle(
-                      fontSize: 16.0,
-                    ),
-                  ),
-                  Divider(
-                    color: Colors.grey,
-                    height: 1.0,
-                  ),
-                ],
-              ),
-            ],
-          ),
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundColor: Colors.black,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+          fontSize: 16.0,
         ),
-      ],
+      ),
     );
+    // Container(
+    //   margin: EdgeInsets.symmetric(vertical: 10.0),
+    //   child: Row(
+    //     children: [
+    //       SizedBox(
+    //         width: 10,
+    //       ),
+    //       CircleAvatar(
+    //         backgroundColor: Colors.black,
+    //       ),
+    //       SizedBox(width: 10.0),
+    //       Text(
+    //         title,
+    //         style: TextStyle(
+    //           fontSize: 16.0,
+    //         ),
+    //       ),
+    //     ],
+    //   ),
+    // );
   }
 
   Widget _buildActionTile(String title) {
-    return Container(
-      child: ListTile(
-        title: Text(
-          title,
-          style: TextStyle(color: Colors.red, fontSize: 16),
-        ),
-        onTap: () {
-          // Handle tile tap
-        },
-        dense: true,
+    return ListTile(
+      title: Text(
+        title,
+        style: TextStyle(color: Colors.red, fontSize: 16),
       ),
+      onTap: () {
+        // Handle tile tap
+      },
+      dense: true,
     );
   }
 }
